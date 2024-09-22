febb5141e4a5bcce4bd97f30a8d90270e368ef23
Dblast101 <domo.kh@gmail.com>
2024-09-23 01:41:33 +1000

    D: Created Asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 
 def main():
@@ -10,8 +12,12 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -24,7 +30,7 @@ def main():
 
         for cursor in updatable:
             cursor.update(dt)
-
+        
         screen.fill("black")
         
         for cursor in drawable:
